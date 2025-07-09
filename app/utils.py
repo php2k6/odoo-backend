8c2145d18ae223cda3d9685c323da6435cc1d9cc
Prabhav Patel <php2k6@gmail.com>
2025-07-09 21:26:01 +0530

    added router folder and chatbot functionality

---
+++ app/utils.py
@@ -0,0 +1,20 @@
+from g4f.client import Client
+import g4f
+import g4f.Provider
+import sys
+import os
+
+def get_query_responce(query):
+    client = Client()
+    test_model = "gpt-4o"
+    test_prompt = query
+    with open("system_prompt.txt", "r", encoding="utf-8") as f:
+        system_msg = f.read()
+    test_provider = g4f.Provider.PollinationsAI
+    response = client.chat.completions.create(
+        model=test_model,
+        messages=[{ "role":"system", "content" : system_msg},{"role": "user", "content": test_prompt}],
+        web_search=False,
+        provider=test_provider
+    )
+    return response.choices[0].message.content
