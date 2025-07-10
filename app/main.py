9eb20f9fce2696a190137a18010d538afc361cb2
Prabhav Patel <php2k6@gmail.com>
2025-07-10 18:32:25 +0530

    added CORS permission

--- app/main.py
+++ app/main.py
@@ -3,11 +3,24 @@ from app.database import engine, get_db
 from sqlalchemy.orm import Session
 import app.models
 from app.routers import chatbot
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 #optional to create all tables
 #models.Base.metadata.create_all(bind=engine)
 app.include_router(chatbot.router)
 
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          
+    allow_credentials=True,         
+    allow_methods=["*"],
+    allow_headers=["*"],   
+)
 @app.get("/")
 def root():
     return {"message" : "Hello World"}
